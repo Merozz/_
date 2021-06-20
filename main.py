5d8f5df469e9b1e68e8412bbcc7fe1fa79622838
Merozz <75943179+Merozz@users.noreply.github.com>
2021-06-20 11:54:15 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,72 @@
+class Rooms(object):
+    class director:
+        type = "director"
+        number = 0
+        lv = 4
+    class teachers:
+        type = "teachers room"
+        number = 1
+        lv = 3
+    class studying:
+        type = "studying room"
+        number = 2
+        lv = 2
+    class act:
+        type = "act"
+        number = 3
+        lv = 1
+
+    room0 = director
+    room1 = teachers
+    room2 = studying
+    room3 = act
+    roomsmass = [room0,room1,room2,room3]
+class User(object):
+    class student:
+        type = "student"
+        lv = 2
+        def __init__(self, name, surname, strange):
+            self.name = name
+            self.surname = surname
+            self.strange = strange
+    class teacher(student):
+        type = "teacher"
+        lv = 3
+    class parent(student):
+        type = "visitor"
+        lv = 1
+    class director(student):
+        type = "director"
+        lv = 4
+
+    user1 = student("Арсений", "Воронин", "Тотемное животное СУНЦ УрФУ")
+    user2 = parent("Федор", "Томлиов", "Любопытный владелец Арсения Воронина")
+    user3 = teacher("Нина", "Гейн", "Перехвалила Данила Клюкина")
+    user4 = director("Dungeon", "Master", "Съедает 3 киндер буено в день")
+    usermass = [user1, user2, user3, user4]
+def showUserInfo(user):
+    print(user.type, user.name, user.surname, user.strange)
+def showRoomInfo(room):
+    print(room.type, room.number, room.lv)
+def check(user, room):
+    if user.lv >= room.lv:
+        print(f"{user.type} {user.name} {user.surname} пытается войти в {room.type} {room.number}: успех")
+    else:
+        print(f"{user.type} {user.name} {user.surname} пытается войти в {room.type} {room.number}: провал")
+for i in range(len(User.usermass)):
+    showUserInfo((User.usermass)[i])
+for i in range(len(Rooms.roomsmass)):
+    showRoomInfo((Rooms.roomsmass)[i])
+for i in range(len(User.usermass)):
+    for j in range(len(Rooms.roomsmass)):
+        check(User.usermass[i], Rooms.roomsmass[j])
+
+
+
+
+
+
+
+
+
+
